13d1b2955b7f96e90e68a5a8efaa0ec470da50e1
lhz <pepitsizous@zoho.com>
2021-11-09 20:37:03 +0200

    restructure routes, add comments about impl

--- main.go
+++ main.go
@@ -39,12 +39,19 @@ type UserId struct {
 
 type Game struct {
 	GameId         string    `json:"game_id"`
-	Players        []*UserId `json:"players"`
+	//rethink active/inactive players thing
+	ActivePlayers        []*UserId `json:"players"`
+	InvitedPlayers        []*UserId `json:"invited_players"`
 	ActivityStatus bool      `json:"activity_status"`
 	State          GameState `json:"game_state"`
 }
 
 type GameState struct{}
+
+var testUsers []*User
+var testUserIds []*UserId
+var testPlayers []*UserId
+var testGames []*Game
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	log.Println("createUser called")
@@ -79,25 +86,41 @@ func createGame(w http.ResponseWriter, r *http.Request) {
 	g := &Game{
 		GameId:         shortid.MustGenerate(),
 		ActivityStatus: true,
+		GameState:      nil,
 	}
 	g.Players = append(g.Players, uid)
-	//generate player-specific game code to return
+	//TODO:generate player-specific game code to return
 	json.NewEncoder(w).Encode(g)
 }
 
-func inviteToGame(w http.ResponseWriter, r *http.Request) {}
-func joinGame(w http.ResponseWriter, r *http.Request)     {}
-func playInGame(w http.ResponseWriter, r *http.Request)   {}
+func inviteToGame(w http.ResponseWriter, r *http.Request) {
+	//TODO:for each game's game_id if it equals provided game_id
+	//game.InvitedPlayers append requesting player so they can join
+}
+
+func joinGame(w http.ResponseWriter, r *http.Request) {
+	//TODO:for gamelist if game's game_id equals provided game_id
+	//game.ActivePlayers append requesting player so they can join
+}
+
+func playInGame(w http.ResponseWriter, r *http.Request) {
+}
 
 func main() {
 	// Set up router
 	router := mux.NewRouter()
-	router.HandleFunc("/user/register", createUser)
-	router.HandleFunc("/game/new", createGame)
-	router.HandleFunc("/game/{game_id}", getGameState)
-	router.HandleFunc("/game/invite/{username}", inviteToGame)
-	router.HandleFunc("/game/{game_id}/join", joinGame)
-	router.HandleFunc("/game/{game_id}/play", playInGame)
+	// Set up subrouter for user functions
+	userAPI := router.PathPrefix("/user").Subrouter()
+	// Set up subrouter for game functions
+	gameAPI := router.PathPrefix("/game").Subrouter()
+	// Set up routes for user API
+	userAPI.HandleFunc("/register", createUser)
+	// Set up routes for user API
+	router.HandleFunc("/new", createGame)
+	router.HandleFunc("/{game_id}", getGameState)
+	router.HandleFunc("/{game_id}/join", joinGame)
+	router.HandleFunc("/{game_id}/play", playInGame)
+	router.HandleFunc("/{game_id}/invite/{username}", inviteToGame)
 	log.Println("starting server at port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
