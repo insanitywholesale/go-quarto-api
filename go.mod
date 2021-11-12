850122efc7ade458d0563441bf89224b272f00a6
lhz <pepitsizous@zoho.com>
2021-11-12 11:20:14 +0200

    changes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.com/insanitywholesale/adise1941
 go 1.16
 
 require (
+	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/gorilla/mux v1.8.0
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/Pallinder/go-randomdata"
 	"github.com/gorilla/mux"
 	"github.com/teris-io/shortid"
 	"log"
@@ -11,13 +12,125 @@ import (
 
 var (
 	httpPort string
+	//commit date + hash vars
 )
+
+//Variable of all Quarto pieces
+var AllQuartoPieces = []*QuartoPiece{
+	//All false
+	&QuartoPiece{
+		Dark:   false,
+		Short:  false,
+		Hollow: false,
+		Round:  false,
+	},
+	//One true
+	&QuartoPiece{
+		Dark:   true,
+		Short:  false,
+		Hollow: false,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  true,
+		Hollow: false,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  false,
+		Hollow: true,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  false,
+		Hollow: false,
+		Round:  true,
+	},
+	//Two true
+	&QuartoPiece{
+		Dark:   true,
+		Short:  true,
+		Hollow: false,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  true,
+		Hollow: true,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  true,
+		Hollow: false,
+		Round:  true,
+	},
+	&QuartoPiece{
+		Dark:   true,
+		Short:  false,
+		Hollow: true,
+		Round:  false,
+	},
+	&QuartoPiece{
+		Dark:   true,
+		Short:  false,
+		Hollow: false,
+		Round:  true,
+	},
+	&QuartoPiece{
+		Dark:   false,
+		Short:  false,
+		Hollow: true,
+		Round:  true,
+	},
+	//Three true
+	&QuartoPiece{
+		Dark:   false,
+		Short:  true,
+		Hollow: true,
+		Round:  true,
+	},
+	&QuartoPiece{
+		Dark:   true,
+		Short:  false,
+		Hollow: true,
+		Round:  true,
+	},
+	&QuartoPiece{
+		Dark:   true,
+		Short:  true,
+		Hollow: false,
+		Round:  true,
+	},
+	&QuartoPiece{
+		Dark:   true,
+		Short:  true,
+		Hollow: true,
+		Round:  false,
+	},
+	//All true
+	&QuartoPiece{
+		Dark:   true,
+		Short:  true,
+		Hollow: true,
+		Round:  true,
+	},
+}
 
 // Constant for Bad Request
 const BadReq string = `{"error": "bad request"}`
 
 // Constant for Not Found
 const NotFound string = `{"error": "not found"}`
+
+// Constant for User Not Found
+const UserNotFound string = `{"error": "user not found"}`
+
+// Constant for Game Not Found
+const GameNotFound string = `{"error": "game not found"}`
 
 type User struct {
 	UserName string `json:"username"`
@@ -34,12 +147,16 @@ type Game struct {
 	GameId         string    `json:"game_id"`
 	ActivePlayers  []*UserId `json:"players"`
 	InvitedPlayers []*UserId `json:"invited_players"`
+	PlayerTurn     *UserId   `json:"next_player"`
 	ActivityStatus bool      `json:"activity_status"`
 	State          GameState `json:"game_state"`
 }
 
 //TODO: fill in with fields
-type GameState struct{}
+type GameState struct {
+	Board        [4][4]*QuartoPiece
+	UnusedPieces []*QuartoPiece
+}
 
 type QuartoPiece struct {
 	Dark   bool
@@ -64,11 +181,14 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(BadReq))
 		return
 	}
+	//TODO: replace with db call
+	testUsers = append(testUsers, u)
 	uid := &UserId{
 		UserName: u.UserName,
 		UserId:   shortid.MustGenerate(),
 	}
-	//TODO: save uid to storage
+	//TODO: replace with db call
+	testUserIds = append(testUserIds, uid)
 	json.NewEncoder(w).Encode(uid)
 }
 
@@ -84,6 +204,7 @@ func getGameState(w http.ResponseWriter, r *http.Request) {
 func createGame(w http.ResponseWriter, r *http.Request) {
 	log.Println("createGame called")
 	w.Header().Set("Content-Type", "application/json")
+	//user that creates the game
 	uid := &UserId{}
 	err := json.NewDecoder(r.Body).Decode(uid)
 	if err != nil {
@@ -91,35 +212,134 @@ func createGame(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(BadReq))
 		return
 	}
+	//create a new game instance
 	g := &Game{
 		GameId:         shortid.MustGenerate(),
 		ActivityStatus: true,
 	}
+	//automatically invite the game creator to the game
 	g.InvitedPlayers = append(g.InvitedPlayers, uid)
+	//TODO: replace with db call
+	testGames = append(testGames, g)
 	//TODO: generate player-specific game code to return
 	json.NewEncoder(w).Encode(g)
 }
 
 func inviteToGame(w http.ResponseWriter, r *http.Request) {
-	//TODO: for each game's game_id if it equals provided game_id
-	//game.InvitedPlayers append requesting player so they can join
+	log.Println("inviteToGame called")
+	w.Header().Set("Content-Type", "application/json")
+	//get the path parameters
+	params := mux.Vars(r)
+	//get game_id from path param
+	gameId, _ := params["game_id"]
+	log.Println("gameId", gameId)
+
+	//user to be invited
+	var uid *UserId = nil
+	//get the name of the user to be invited from path param
+	inviteeName, _ := params["username"]
+	log.Println("inviteeName", inviteeName)
+	//see if user exists in the user database
+	for idx, u := range testUserIds {
+		if u.UserName == inviteeName {
+			uid = testUserIds[idx]
+			break
+		}
+	}
+	//return error if user with username can't be found
+	if uid == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(GameNotFound))
+		return
+	}
+	//append player to game if game exists
+	for _, g := range testGames {
+		if g.GameId == gameId {
+			g.InvitedPlayers = append(g.InvitedPlayers, uid)
+			w.WriteHeader(http.StatusOK)
+			//TODO: some success message
+			return
+		}
+	}
+	//return error if game doesn't exist
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(GameNotFound))
+	return
 }
 
 func joinGame(w http.ResponseWriter, r *http.Request) {
-	//TODO: for gamelist if game's game_id equals provided game_id
-	//game.ActivePlayers append requesting player so they can join
+	log.Println("joinGame called")
+	w.Header().Set("Content-Type", "application/json")
+	//get the path parameters
+	params := mux.Vars(r)
+	//get game_id from path param
+	gameId, _ := params["game_id"]
+	log.Println("gameId", gameId)
+
+	//user trying to join
+	uid := &UserId{}
+	err := json.NewDecoder(r.Body).Decode(uid)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(BadReq))
+		return
+	}
+
+	//TODO: error messages in following loop
+	//TODO: returns in following loop
+	for _, g := range testGames {
+		if g.GameId == gameId {
+			for idx, u := range g.InactivedPlayers {
+				if uid.UserId == u.UserId {
+					//TODO: don't append if the lobby is full
+					g.ActivePlayers = append(g.ActivePlayers, u)
+					//TODO: delete from g.InactivePlayers
+				}
+			}
+		}
+	}
+
 }
 
 func playInGame(w http.ResponseWriter, r *http.Request) {
+	log.Println("playInGame called")
+	w.Header().Set("Content-Type", "application/json")
 	//TODO: depends on game, will probably be quarto
+}
+
+func checkGameState(gameId string) {
+	//TODO: for gamelist if game's game_id equals provided game_id
+	//assign game.State to gameState
+	var gameState GameState
+	for _, g := range testGames {
+		if g.GameId == gameId {
+			gameState = g.State
+		}
+	}
+
+	board := gameState.Board
+	for k, v := range board {
+		log.Println(k, v)
+	}
 }
 
 // Function to set server HTTP port
 func setupHTTPPort() {
 	httpPort = os.Getenv("QUARTO_HTTP_PORT")
 	if httpPort == "" {
-		httpPort = "8080"
+		httpPort = "8000"
 	}
+}
+
+// Only for testing
+func genRandomPiece() *QuartoPiece {
+	qp := &QuartoPiece{
+		Dark:   randomdata.Boolean(),
+		Short:  randomdata.Boolean(),
+		Hollow: randomdata.Boolean(),
+		Round:  randomdata.Boolean(),
+	}
+	return qp
 }
 
 func main() {
@@ -138,9 +358,9 @@ func main() {
 	gameRouter.HandleFunc("/{game_id}/play", playInGame)
 	gameRouter.HandleFunc("/{game_id}/invite/{username}", inviteToGame)
 	// Determine port to run at
-	httpPort()
+	setupHTTPPort()
 	// Print a message so there is feedback to the app admin
-	log.Println("starting server at port" + httpPort)
+	log.Println("starting server at port", httpPort)
 	// One-liner to start the server or print error
 	log.Fatal(http.ListenAndServe(":"+httpPort, router))
 }
