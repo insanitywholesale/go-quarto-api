1e2ef309e552fd7adabd3baf19b60838e557557b
lhz <pepitsizous@zoho.com>
2021-11-12 23:41:10 +0200

    add a test

---
+++ main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var testUserId string
+
+// Test creating a user
+func TestCreateUser(t *testing.T) {
+	// run test http server
+	testServer := httptest.NewServer(setupRouter())
+
+	// define URL
+	testURL := testServer.URL + "/user/register"
+
+	// create some data in the form of an io.Reader from a string of json
+	jsonData := []byte(`{"username": "myself", "password": "my password"}`)
+
+	// do a simple Post request with the above data
+	res, err := http.Post(testURL, "application/json", bytes.NewBuffer(jsonData))
+
+	// check for request errors
+	if err != nil {
+		t.Error("POST error:", err)
+	}
+
+	// be responsible and close the response some time
+	defer res.Body.Close()
+
+	// log response
+	t.Log(res)
+
+	// save response body to check later
+	body, err := io.ReadAll(res.Body)
+
+	// check for response body read errors
+	if err != nil {
+		t.Error("resp.Body error:", err)
+	}
+
+	// log response body
+	t.Log(string(body))
+
+	// response should contain json that can maps to the UserId type
+
+	// set up empty UserId
+	u := &UserId{}
+
+	// try to unmarshal
+	err = json.Unmarshal(body, u)
+
+	// check for unmarshaling errors
+	if err != nil {
+		t.Error("unmarshal error:", err)
+	}
+
+	// log UserId
+	t.Log(u)
+}
